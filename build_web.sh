36229f68d24479a6e3c52a6e212597b54b667380
Karl Zylinski <karl@zylinski.se>
2025-01-03 22:22:25 +0100

    Fixes for linux build script

--- build_web.sh
+++ build_web.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-EMSCRIPTEN_SDK_DIR="$HOME/emsdk"
+# This one is optional on some Linux systems if you've installed emscripten through a package manager.
+EMSCRIPTEN_SDK_DIR="$HOME/repos/emsdk"
 OUT_DIR="build/web"
 
 mkdir -p $OUT_DIR
@@ -14,11 +15,10 @@ if ! odin build main_web -target:freestanding_wasm32 -build-mode:obj -define:RAY
 fi
 
 ODIN_PATH=$(odin root)
-files="main_web/main_web.c $OUT_DIR/game.wasm.o $ODIN_PATH/vendor/raylib/wasm/libraylib.a $ODIN_PATH/vendor/raylib/wasm/libraygui.a"
+files="main_web/main_web.c $OUT_DIR/game.wasm.o ${ODIN_PATH}/vendor/raylib/wasm/libraylib.a ${ODIN_PATH}vendor/raylib/wasm/libraygui.a"
 flags="-sUSE_GLFW=3 -sASYNCIFY -sASSERTIONS -DPLATFORM_WEB"
 custom="--shell-file main_web/index_template.html --preload-file assets"
 
 # shellcheck disable=SC2086
 # Add `-g` to `emcc` call to enable debug symbols (works in chrome).
 emcc -o $OUT_DIR/index.html $files $flags $custom && rm $OUT_DIR/game.wasm.o
-cp -R ./assets/ ./$OUT_DIR/assets/
